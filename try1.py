a487a67ef5ec010b540a23b925bcfb1111eda083
fahad <fa.alsaadi99@gmail.com>
2024-08-12 02:23:05 +0300

    work2

---
+++ try1.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+#import arabic_reshaper as ar 
+
+st.markdown("""
+    <style>
+    .custom-title {
+        font-size: 70px;
+        color: blue;
+        text-align: center;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.markdown('<p class="custom-title">دوبك متخرج و شايل هم البحث عن وظيفة؟</p>', unsafe_allow_html=True)
+st.write('')
+st.write('')
+st.write('')
+st.header('ولا يهمك ضبطناك في دليل كامل فيه كل ما تحتاجه ')
+st.image(r"C:\Users\foooo\OneDrive\سطح المكتب\photo pro\joey-friends-friends.gif", use_column_width=True)
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.header('في البداية تحب تأخذ فكرة عن توزيع الوظايف في اللمملكة العربية السعودية ؟')
+st.write("")
+st.image(r"C:\Users\foooo\OneDrive\سطح المكتب\photo pro\fig_1.png") 
+st.write("")
+st.write("") 
+st.header('زي ما هو واضحة قدامك العاصمة متصدرة و اكله النص تقريبا على بقية المناطق')
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.header('طيب تحب او تحبين تلقي نظرة على احتياج السوق بناء على الجنس و لا ما تعقد او تعتقدين فين فرق ؟')
+st.write("")
+st.write("")
+st.image(r"C:\Users\foooo\OneDrive\سطح المكتب\photo pro\fig_2.png")
+st.write("")
+st.write("")
+st.header('طحت في الفخ و خسرناك في نقاش الطفولة مين اقوى الاولاد و لا البنات و خليناها تعادل ')
+st.write("")
+st.image(r"C:\Users\foooo\OneDrive\سطح المكتب\photo pro\tumblr_m3bfohqL9f1rqfhi2o1_400.gif") 
+st.write("")
+st.write("")
+st.write("")
+st.header('هنا الحقيقة و تقدر تستنج مين اقوى براحتك ')
+st.image(r"C:\Users\foooo\OneDrive\سطح المكتب\photo pro\gif3.png")
+st.write("")
+st.write("")
+st.write("")
+st.header('ختاما السوق يقول فرصتك اكبر من الخبير')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image(r"C:\Users\foooo\OneDrive\سطح المكتب\photo pro\Screenshot 2024-08-12 021237.png")
+
+with col2:
+    st.image(r"C:\Users\foooo\OneDrive\سطح المكتب\photo pro\ba510aaca11f36bcc64738c8cc59d1c5_w200.gif")
